55a95ed81a10a10223058e755189c36c202f51e3
Jo Franchetti <jofranchetti@gmail.com>
2024-11-10 19:13:43 +0000

    style

--- client/src/components/DeploymentViewer.tsx
+++ client/src/components/DeploymentViewer.tsx
@@ -12,9 +12,11 @@ export default function DeploymentViewer(props: Props) {
   return (
     <div className="console">
       <div className="url">
-        {showPreview ? <a href={url}>{url}</a> : "Deployment URL"}         
+        {showPreview ? <a href={url}>{url}</a> : "Deployment URL"}
       </div>
-      {showPreview ? <iframe src={url} title="sandbox" className="sandbox" /> :  <p className="sandbox">{status}</p>}
+      {showPreview
+        ? <iframe src={url} title="sandbox" className="sandbox" />
+        : <p className="sandbox">{status}</p>}
     </div>
-  )
+  );
 }
